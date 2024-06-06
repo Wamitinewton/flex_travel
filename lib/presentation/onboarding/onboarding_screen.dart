44eccf9992e7fd123a1ff3ab9081515f24590f9a
Wamitinewton <wamitinewton@gmail.com>
2024-06-06 15:17:46 +0300

    some few changes

---
+++ lib/presentation/onboarding/onboarding_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class OnboardingScreen extends StatefulWidget {
+  const OnboardingScreen({super.key});
+
+  @override
+  State<OnboardingScreen> createState() => _OnboardingScreenState();
+}
+
+class _OnboardingScreenState extends State<OnboardingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('assets/images/pic2.jpg'),
+                fit: BoxFit.cover)),
+        child: Stack(
+          children: [
+            Positioned(
+                bottom: 0,
+                child: Container(
+                  height: 280,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20)),
+                ))
+          ],
+        ),
+      ),
+    );
+  }
+}
